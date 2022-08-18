5619f5cbc23f6301d6679dd1e4f5a4b851a239eb
Muthu <muthu@muthus-MacBook-Pro.local>
2022-08-18 00:50:33 -0400

    black format

--- adapters/out/weather_ddb_service.py
+++ adapters/out/weather_ddb_service.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Optional
 import os
 import boto3
 from ports.out.weather_repository import BaseRepository
+
 if TYPE_CHECKING:
     from mypy_boto3_dynamodb.service_resource import Table
 from datetime import datetime
@@ -21,5 +22,7 @@ class WeatherDdbService(BaseRepository):
 
     def save_weather_info(self, data: dict[str, str]) -> None:
         assert self.table is not None
-        self.table.put_item(Item={"pk": data["stateCd"], "sk": str(datetime.now()), **data})
+        self.table.put_item(
+            Item={"pk": data["stateCd"], "sk": str(datetime.now()), **data}
+        )
         pass
