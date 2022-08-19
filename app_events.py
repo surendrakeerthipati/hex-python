a4f30d2694810d8ddb3b03612a797088d60a808f
Muthu <muthu@muthus-MacBook-Pro.local>
2022-08-19 01:12:15 -0400

    simplify eventbridge events

--- app_events.py
+++ app_events.py
@@ -5,7 +5,8 @@ from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.data_classes import (EventBridgeEvent,
                                                           event_source)
 
-from adapters.eventbridge_adapter import event_handler
+from adapters.out.weather_ddb_service import WeatherDdbService
+from domain.hex_manager import DomainManager
 
 os.environ["POWERTOOLS_SERVICE_NAME"] = "hex-event-handler"
 
@@ -13,9 +14,20 @@ tracer = Tracer()
 logger = Logger()
 
 
+domain_mgr = None  # declared as global vars - for init purposes
+
+
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.EVENT_BRIDGE)
 @tracer.capture_lambda_handler
 @event_source(data_class=EventBridgeEvent)
 def lambda_handler(event: EventBridgeEvent, context):
     logger.info(f"Received {event.detail_type} with detail {event.detail}")
-    event_handler(event=event)
+    global domain_mgr
+    if domain_mgr is None:
+        domain_mgr = DomainManager(weather_repo=WeatherDdbService.from_env())
+    if event.detail_type == "Weather Alert":
+        domain_mgr.save_weather_info(event.detail)
+    else:
+        logger.warn(
+            f"Unknown Event Received, Implement an handler for detail-type {event.detail_type}"
+        )
